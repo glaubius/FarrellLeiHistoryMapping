c00788c519a2749104b009ab15657aad4ff796ed
Jen Glaubius <glaubius@gmail.com>
2020-03-11 13:55:24 -0400

    add leaflet map

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+console.log('L', L)
 
+const position = [44.12, -99.2];
+const map = L.map('map').setView(position, 7);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+console.log('map', map)
